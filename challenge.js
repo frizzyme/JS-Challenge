85ef484bc70271fc44788c0640e9be218aad3511
Melinda <vargamelinda21@gmail.com>
2022-11-27 23:55:41 +0100

    correction

--- challenge.js
+++ challenge.js
@@ -58,34 +58,38 @@ function getNumberFromConsole() {
 async function displayInConsole() {
     let numberFromConsole;
 
-    console.log("### 1- Mostrar en formato de tabla todos los alumnos.\n\
-    ### 2- Mostrar por consola la cantidad de alumnos que hay en clase.\n\
-    ### 3- Mostrar por consola todos los nombres de los alumnos.\n\
-    ### 4- Eliminar el último alumno de la clase.\n\
-    ### 5- Eliminar un alumno aleatoriamente de la clase.\n\
-    ### 6- Mostrar por consola todos los datos de los alumnos que son chicas.\n\
-    ### 7- Mostrar por consola el número de chicos y chicas que hay en la clase.\n\
-    ### 8- Mostrar true o false por consola si todos los alumnos de la clase son chicas.\n\
-    ### 9- Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.\n\
-    ### 10- Añadir un alumno nuevo con los siguientes datos:\n\
-    - nombre aleatorio.\n\
-    - edad aleatoria entre 20 y 50 años.\n\
-    - género aleatorio.\n\
-    - listado de calificaciones vacío.\n\
-    ### 11- Mostrar por consola el nombre de la persona más joven de la clase.\n\
-    ¡OJO!, si varias personas de la clase comparten la edad más baja, cualquiera de ellos es una respuesta válida.\n\
-    ### 12- Mostrar por consola la edad media de todos los alumnos de la clase.\n\
-    ### 13- Mostrar por consola la edad media de las chicas de la clase.\n\
-    ### 14- Añadir nueva nota a los alumnos. Por cada alumno de la clase, tendremos que calcular una nota de forma aleatoria(número entre 0 y 10) y añadirla a su listado de notas.\n\
-    ### 15- Ordenar el array de alumnos alfabéticamente según su nombre."); 
-
+  
     do {
+
+      console.log("### 1- Mostrar en formato de tabla todos los alumnos.\n\
+      ### 2- Mostrar por consola la cantidad de alumnos que hay en clase.\n\
+      ### 3- Mostrar por consola todos los nombres de los alumnos.\n\
+      ### 4- Eliminar el último alumno de la clase.\n\
+      ### 5- Eliminar un alumno aleatoriamente de la clase.\n\
+      ### 6- Mostrar por consola todos los datos de los alumnos que son chicas.\n\
+      ### 7- Mostrar por consola el número de chicos y chicas que hay en la clase.\n\
+      ### 8- Mostrar true o false por consola si todos los alumnos de la clase son chicas.\n\
+      ### 9- Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.\n\
+      ### 10- Añadir un alumno nuevo con los siguientes datos:\n\
+      - nombre aleatorio.\n\
+      - edad aleatoria entre 20 y 50 años.\n\
+      - género aleatorio.\n\
+      - listado de calificaciones vacío.\n\
+      ### 11- Mostrar por consola el nombre de la persona más joven de la clase.\n\
+      ¡OJO!, si varias personas de la clase comparten la edad más baja, cualquiera de ellos es una respuesta válida.\n\
+      ### 12- Mostrar por consola la edad media de todos los alumnos de la clase.\n\
+      ### 13- Mostrar por consola la edad media de las chicas de la clase.\n\
+      ### 14- Añadir nueva nota a los alumnos. Por cada alumno de la clase, tendremos que calcular una nota de forma aleatoria(número entre 0 y 10) y añadirla a su listado de notas.\n\
+      ### 15- Ordenar el array de alumnos alfabéticamente según su nombre."); 
+
+
         try {
             numberFromConsole = await getNumberFromConsole()
         } catch (error) {
             console.log(error)
             process.exit(0)
         }
+
 
         const femaleStudents = students.filter(student => student.gender === 'female');
         const maleStudents = students.filter(student => student.gender === 'male');
